9b168dbc7adae2975f9db02f6cbb505994ed9ed9
ALIBI Ghazi <123127137+GhaziAlibi@users.noreply.github.com>
2025-08-09 12:08:37 +0200

    -config for ionic - config for gnupg

--- zsh/.config/zsh/environment.zsh
+++ zsh/.config/zsh/environment.zsh
@@ -16,3 +16,27 @@ if [ -d "$HOME/.local/bin" ]; then
 fi
 
 export PATH="$PATH:$HOME/.dotnet/tools"
+
+
+# =================
+# Android
+# =================
+export ANDROID_SDK_ROOT="$HOME/Android/Sdk"
+export PATH="$ANDROID_SDK_ROOT/platform-tools:$ANDROID_SDK_ROOT/cmdline-tools/latest/bin:$PATH"
+
+# =================
+# JAVA
+# =================
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk
+export PATH=$JAVA_HOME/bin:$PATH
+
+# =================
+# CAPACITOR
+# =================
+export CAPACITOR_ANDROID_STUDIO_PATH="/home/ghazi/.local/bin/launch-android-studio"
+
+
+# =================
+# GNUPG
+# =================
+export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
